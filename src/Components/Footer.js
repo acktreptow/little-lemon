c1ae2eef908550077ac3958de8c4e6ddc2e54c15
acktreptow <acktreptow@gmail.com>
2023-10-03 18:49:11 +0100

    update links

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -11,25 +11,25 @@ function Footer() {
       <div className="footer-links">
         <div className="footer-navigation-container">
           <h1>Navigation</h1>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/menu">Menu</Link>
-          <Link to="/reservations">Reservations</Link>
-          <Link to="/order-online">Order Online</Link>
-          <Link to="/login">Login</Link>
+          <Link to="/little-lemon">Home</Link>
+          <Link to="/little-lemon/about">About</Link>
+          <Link to="/little-lemon/menu">Menu</Link>
+          <Link to="/little-lemon/reservations">Reservations</Link>
+          <Link to="/little-lemon/order-online">Order Online</Link>
+          <Link to="/little-lemon/login">Login</Link>
         </div>
         <div className="footer-contact-container">
           <h1>Contact</h1>
-          <Link to="/contact">Address</Link>
-          <Link to="/contact">Phone Number</Link>
-          <Link to="/contact">Email</Link>
+          <Link to="/little-lemon/contact">Address</Link>
+          <Link to="/little-lemon/contact">Phone Number</Link>
+          <Link to="/little-lemon/contact">Email</Link>
         </div>
         <div className="footer-socialMedia-container">
           <h1>Social Media</h1>
-          <Link to="/contact">Facebook</Link>
-          <Link to="/contact">Instagram</Link>
-          <Link to="/contact">Twitter</Link>
-          <Link to="/contact">YouTube</Link>
+          <Link to="/little-lemon/contact">Facebook</Link>
+          <Link to="/little-lemon/contact">Instagram</Link>
+          <Link to="/little-lemon/contact">Twitter</Link>
+          <Link to="/little-lemon/contact">YouTube</Link>
         </div>
       </div>
     </footer>
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,12 +4,12 @@ function Nav() {
   return (
     <>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/menu">Menu</Link>
-        <Link to="/reservations">Reservations</Link>
-        <Link to="/order-online">Order Online</Link>
-        <Link to="/login">Login</Link>
+        <Link to="/little-lemon">Home</Link>
+        <Link to="/little-lemon/about">About</Link>
+        <Link to="/little-lemon/menu">Menu</Link>
+        <Link to="/little-lemon/reservations">Reservations</Link>
+        <Link to="/little-lemon/order-online">Order Online</Link>
+        <Link to="/little-lemon/login">Login</Link>
       </nav>
     </>
   );
