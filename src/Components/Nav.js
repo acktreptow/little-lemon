9f6ac4ed3fc083d90f35fd5385cc971eda564975
acktreptow <acktreptow@gmail.com>
2023-11-10 18:28:26 +0000

    update header

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -8,7 +8,7 @@ function Nav() {
         <Link to="/little-lemon/about">About</Link>
         <Link to="/little-lemon/menu">Menu</Link>
         <Link to="/little-lemon/reservations">Reservations</Link>
-        <Link to="/little-lemon/order-online">Order Online</Link>
+        <Link to="/little-lemon/order-online">Order</Link>
         <Link to="/little-lemon/login">Login</Link>
       </nav>
     </>
