df9c60f8d2b6e79d2040f554f6df2ec4ad6e458c
acktreptow <acktreptow@gmail.com>
2024-02-12 19:48:31 +0000

    update logo link

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,14 @@
 import logo from "../assets/Logo.svg";
 import Nav from "./Nav";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
   return (
     <header className="header-container">
-      <img src={logo} alt="Little Lemon's Logo" />
+      <Link to="/little-lemon">
+        <img src={logo} alt="Little Lemon's Logo" />
+      </Link>
       <Nav />
     </header>
   );
