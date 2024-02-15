fe7352fa8f12efd7f336d5fa61efea77942981cf
acktreptow <acktreptow@gmail.com>
2024-02-14 22:18:31 -0700

    update navigation

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,18 @@
 import logo from "../assets/Logo.svg";
 import Nav from "./Nav";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
+  const navigate = useNavigate();
+
   return (
     <header className="header-container">
-      <Link to="/little-lemon">
-        <img src={logo} alt="Little Lemon's Logo" />
-      </Link>
+      <img
+        src={logo}
+        alt="Little Lemon's Logo"
+        onClick={() => navigate("/little-lemon")}
+      />
       <Nav />
     </header>
   );
