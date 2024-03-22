82775ede119e42468b636f17fd759854b0de1819
acktreptow <acktreptow@gmail.com>
2024-03-21 21:07:17 -0700

    update wrong page

--- src/pages/Others/WrongPage.js
+++ src/pages/Others/WrongPage.js
@@ -6,7 +6,7 @@ function WrongPage() {
     <div className="page-container">
       <h1 className="title">This isn't the page you're looking for!</h1>
       <h2 className="sub-title">
-        (Click either 'Home' or 'Reservations' in the navbar above for relevant
+        (Click either Home or Reservations in the navbar above for relevant
         parts of the project)
       </h2>
       <img src={starWars} className="star-wars-img" alt="Star Wars meme" />
